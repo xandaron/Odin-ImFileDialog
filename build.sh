689fc902f3f8a1d2590734ce941b73a600c63467
Alex Davis <xandaron@gmail.com>
2025-01-14 14:56:27 +0000

    new build.sh script for linux

---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# BASEDIR=$(dirname "$0")
+clang++ -std=c++17 -Wall -Wextra -c c_wrapper.cpp ImFileDialog/ImFileDialog.cpp -I"imgui/imgui.h" -I"imgui/imgui_internal.h" -I"stb_image/stb_image.h"
+ar rc ImFileDialog.a c_wrapper.o ImFileDialog.o
+rm c_wrapper.o ImFileDialog.o
